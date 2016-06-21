bb4c913797f793beda5158cb6d149cd8baf64524
Heber Sandoval <he3ers@gmail.com>
2016-06-21 06:52:32 -0400

    User can signup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,39 @@
 class UsersController < ApplicationController
+  def index
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome, your account has been created."
+      redirect_to @user
+    else
+      flash.now[:danger] = "Your account has not been created."
+      render "new"
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   # callback ensures email uniqueness by downcasing the email attribute
   before_save { self.email = email.downcase }
-  validates :name, presence: true, length: { maximim: 25 }
-  validates :email, presence: true, length: { maximim: 50 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }
+  validates :name, presence: true, length: { maximum: 25 }
+  validates :email, presence: true, length: { maximum: 50 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
